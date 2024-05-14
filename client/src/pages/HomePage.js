ab7046673d46148513620c7123748483d35a44f8
zerowasd <neerajkumaryadav01102003@gmail.com>
2024-05-14 09:36:04 +0000

    table-and-chart-icon-added

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,9 @@
-import React,{useState, useEffect} from 'react';
-import {Modal,Form,Input,Select,message,Table,DatePicker} from 'antd';
-import Layout from '../components/Layout/Layout';
-import axios from 'axios';
-import Spinner from './../components/Spinner';
+import React, { useState, useEffect } from "react";
+import { Modal, Form, Input, Select, message, Table, DatePicker } from "antd";
+import { UnorderedListOutlined, AreaChartOutlined } from "@ant-design/icons";
+import Layout from "../components/Layout/Layout";
+import axios from "axios";
+import Spinner from "./../components/Spinner";
 import moment from "moment";
 const { RangePicker } = DatePicker;
 
@@ -10,154 +11,183 @@ const { RangePicker } = DatePicker;
 
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
-  const [loading,setLoading] = useState(false);
-  const [allTransaction, setAllTransaction] = useState([])
-  const [frequency,setFrequency] = useState('7')
-  const [selectedDate, setSelectedDate] = useState([])
-  const [type,setType] = useState('all')
+  const [loading, setLoading] = useState(false);
+  const [allTransaction, setAllTransaction] = useState([]);
+  const [frequency, setFrequency] = useState("7");
+  const [selectedDate, setSelectedDate] = useState([]);
+  const [type, setType] = useState("all");
+  const [viewData, setViewData] = useState("table");
   //table data
-  
+
   const columns = [
     {
-      title:'Date',
-      dataIndex:'date',
-      render : (text) => <span>{moment(text).format('YYYY-MM-DD')}</span>,
+      title: "Date",
+      dataIndex: "date",
+      render: (text) => <span>{moment(text).format("YYYY-MM-DD")}</span>,
     },
     {
-      title:'Amount',
-      dataIndex:'amount'
+      title: "Amount",
+      dataIndex: "amount",
     },
     {
-      title:'Type',
-      dataIndex:'type'
+      title: "Type",
+      dataIndex: "type",
     },
     {
-      title:'Category',
-      dataIndex:'category'
+      title: "Category",
+      dataIndex: "category",
     },
     {
-      title:'Reference',
-      dataIndex:'reference'
+      title: "Reference",
+      dataIndex: "reference",
     },
     {
-      title:'Actions',
+      title: "Actions",
+    },
+  ];
 
-    } 
-  ]
-  
   //use effect hook
   //get all transactions
   useEffect(() => {
-    
-  const getAllTransactions = async () => {
-    try {
-      const user = JSON.parse(localStorage.getItem("user"));
-      setLoading(true);
-      const res = await axios.post("/transactions/get-transaction", {
-        userid: user._id,
-        frequency,
-        selectedDate,
-        type
-      });
-      setLoading(false);
-      setAllTransaction(res.data);
-      console.log(res.data);  
-    } catch (error) {
-      console.log(error);
-      message.error("fetch issue with transaction");
-    }
-  };
+    const getAllTransactions = async () => {
+      try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        setLoading(true);
+        const res = await axios.post("/transactions/get-transaction", {
+          userid: user._id,
+          frequency,
+          selectedDate,
+          type,
+        });
+        setLoading(false);
+        setAllTransaction(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+        message.error("fetch issue with transaction");
+      }
+    };
     getAllTransactions();
-  },[frequency, selectedDate,type])
+  }, [frequency, selectedDate, type]);
   //form handling
   const handleSubmit = async (values) => {
     try {
-      const user = JSON.parse(localStorage.getItem('user'))
-      setLoading(true)
-      await axios.post('/transactions/add-transaction', {...values, userid:user._id})
-      setLoading(false)
-      message.success('Transaction Added')
-      setShowModal(false)
+      const user = JSON.parse(localStorage.getItem("user"));
+      setLoading(true);
+      await axios.post("/transactions/add-transaction", {
+        ...values,
+        userid: user._id,
+      });
+      setLoading(false);
+      message.success("Transaction Added");
+      setShowModal(false);
     } catch (error) {
-      setLoading(false)
-      message.error('failed to add transaction')
+      setLoading(false);
+      message.error("failed to add transaction");
     }
-  }
+  };
   return (
     <Layout>
-      {
-        loading && <Spinner />
-      }
+      {loading && <Spinner />}
       <div className="filter">
         <div>
-        <h6>Select frequency</h6>
-        <Select value={frequency} onChange={(values) => setFrequency(values)}>
-          <Select.Option value="7">Last 1 week</Select.Option>
-          <Select.Option value="30">Last 1 month</Select.Option>
-          <Select.Option value="365">Last 1 year</Select.Option>
-          <Select.Option value="custom">custom</Select.Option>
-        </Select>
-        {frequency === 'custom' && <RangePicker value={selectedDate} onChange={(values) => setSelectedDate(values)}/>}
+          <h6>Select frequency</h6>
+          <Select value={frequency} onChange={(values) => setFrequency(values)}>
+            <Select.Option value="all">All</Select.Option>
+            <Select.Option value="7">Last 1 week</Select.Option>
+            <Select.Option value="30">Last 1 month</Select.Option>
+            <Select.Option value="365">Last 1 year</Select.Option>
+            <Select.Option value="custom">custom</Select.Option>
+          </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(values) => setSelectedDate(values)}
+            />
+          )}
         </div>
         <div>
-        <h6>Select Type (Income/Expense)</h6>
-        <Select value={type} onChange={(values) => setType(values)}>
-          <Select.Option value="all">All</Select.Option>
-          <Select.Option value="income">Income</Select.Option>
-          <Select.Option value="expense">Expense</Select.Option>
-        </Select>
-        {frequency === 'custom' && <RangePicker value={selectedDate} onChange={(values) => setSelectedDate(values)}/>}
-        </div>
-          <button className="btn btn-primary" onClick={() => setShowModal(true)}>Add New</button>
-      </div>
-      <div className="content">
-      <Table columns={columns} dataSource={allTransaction}/>
-      </div>
-      <Modal title = "Add Transaction" 
-      open={showModal}
-      onCancel={() => setShowModal(false)}
-      footer={false} 
-      >
-      <Form layout = "vertical" onFinish={handleSubmit}>
-        <Form.Item label="Amount" name="amount">
-            <Input type="text" />
-        </Form.Item>
-        <Form.Item label="type" name="type">
-          <Select>
+          <h6>Select Type (Income/Expense)</h6>
+          <Select value={type} onChange={(values) => setType(values)}>
+            <Select.Option value="all">All</Select.Option>
             <Select.Option value="income">Income</Select.Option>
             <Select.Option value="expense">Expense</Select.Option>
           </Select>
-        </Form.Item>
-        <Form.Item label="Category" name="category">
-          <Select>
-            <Select.Option value="salary">Salary</Select.Option>
-            <Select.Option value="tip">Tip</Select.Option>
-            <Select.Option value="Side Project">Side Project</Select.Option>
-            <Select.Option value="food">Food</Select.Option>
-            <Select.Option value="movie">Movie</Select.Option>
-            <Select.Option value="medical">Medical</Select.Option>
-            <Select.Option value="fees">Fees</Select.Option>
-            <Select.Option value="tax">Tax</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="Date" name="date">
-          <Input type="date" />
-        </Form.Item>
-        <Form.Item label="Refrence" name="reference">
-          <Input type="text" />
-        </Form.Item>
-        <Form.Item label="Description" name="description">
-          <Input type="text" />
-        </Form.Item>
-        <div className="d-flex justify-content-end">
-          <button type="submit" className="btn btn-primary">
-            Save
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(values) => setSelectedDate(values)}
+            />
+          )}
+        </div>
+        <div className="switch-icons">
+          <UnorderedListOutlined
+            className={`mx-2 ${viewData === 'table' ? 'active-icon' : 'inactive-icon'}`}
+            onClick={() => setViewData("table")}
+          />
+          <AreaChartOutlined
+            className={`mx-2 ${viewData === 'analytics' ? 'active-icon' : 'inactive-icon'}`}
+            onClick={() => setViewData("analytics")}
+          />
+        </div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowModal(true)}
+          >
+            Add New
           </button>
         </div>
-      </Form>
+      </div>
+      <div className="content">
+        <Table columns={columns} dataSource={allTransaction} />
+      </div>
+      <Modal
+        title="Add Transaction"
+        open={showModal}
+        onCancel={() => setShowModal(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item label="Amount" name="amount">
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item label="type" name="type">
+            <Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Category" name="category">
+            <Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="tip">Tip</Select.Option>
+              <Select.Option value="Side Project">Side Project</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="movie">Movie</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="fees">Fees</Select.Option>
+              <Select.Option value="tax">Tax</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <Input type="date" />
+          </Form.Item>
+          <Form.Item label="Refrence" name="reference">
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item label="Description" name="description">
+            <Input type="text" />
+          </Form.Item>
+          <div className="d-flex justify-content-end">
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </Form>
       </Modal>
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
